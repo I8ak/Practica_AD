324b3be2500d98b8df3e917ef8f4808992cf9a03
diurno <alhelimiishak@gmail.com>
2024-12-02 10:56:30 +0100

    copiar el pom

--- src/main/java/org/example/clases/Profesor.java
+++ src/main/java/org/example/clases/Profesor.java
@@ -1,5 +1,61 @@
 package org.example.clases;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.ArrayList;
+
 @Entity
 public class Profesor {
+    @Id
+    private String id;
+    private String nombre;
+    private String apellido;
+    private Asignatura asognatura;
+    private Departamento departamento;
+    private ArrayList<Alumno> alumnos;
 
+    public Profesor(String id, String nombre, String apellido, Asignatura asognatura, Departamento departamento, ArrayList<Alumno> alumnos) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.asognatura = asognatura;
+        this.departamento = departamento;
+        this.alumnos = alumnos;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Profesor{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", asognatura=" + asognatura +
+                ", departamento=" + departamento +
+                ", alumnos=" + alumnos +
+                '}';
+    }
 }
