7271e7b03fd0f8ab23240412a4bcfbf07f55b9ca
LuciaR <lrm390@educa.madrid.org>
2024-12-02 11:00:09 +0100

    a

---
+++ src/main/java/org/example/clases/Alumnos.java
@@ -0,0 +1,57 @@
+package org.example.clases;
+
+
+import java.util.HashMap;
+
+
+public class Alumnos {
+    private String nif;
+    private String nombre;
+    private String apellido;
+    private HashMap<Asignatura>asignaturas;
+    private Clase clase;
+
+    public Alumnos(String nif, String nombre, String apellido, HashMap<Asignatura> asignaturas, Clase clase) {
+        this.nif = nif;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.asignaturas = asignaturas;
+        this.clase = clase;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return "Alumnos{" +
+                "nif='" + nif + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", asignaturas=" + asignaturas +
+                ", clase=" + clase +
+                '}';
+    }
+
+}
