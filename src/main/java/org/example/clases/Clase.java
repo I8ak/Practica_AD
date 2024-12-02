f887068c787b2e13d130a03159b185d4d14b19b7
diurnodam2 <davidyanansang@gmail.com>
2024-12-02 11:00:52 +0100

    se

---
+++ src/main/java/org/example/clases/Clase.java
@@ -0,0 +1,23 @@
+package org.example.clases;
+
+public class Clase {
+    private String codigo;
+
+    public Clase(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Clase{" +
+                "codigo='" + codigo + '\'' +
+                '}';
+    }
+}
